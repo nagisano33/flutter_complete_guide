8bf05675938ee79b430c0a5b57c1898980f81e24
nagisano33 <vita.stars2@gmail.com>
2023-04-15 19:48:22 +0900

    Update statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+@override
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex ^ 1;
+    });
+    print(questionIndex);
+  }
+
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            // RaisedButton
+            ElevatedButton(
+              child: Text('Answoer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answoer 2'),
+              onPressed: () => print('Ans 2 was chosen'),
+            ),
+            ElevatedButton(
+              child: Text('Answoer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
