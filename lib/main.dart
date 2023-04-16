aeeabe3e54201e3526562b6b7a25c1faac8619e0
nagisano33 <vita.stars2@gmail.com>
2023-04-16 15:12:09 +0900

    Update テキストをカスタムウィジェットに差し替え

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -37,7 +39,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             // RaisedButton
             ElevatedButton(
               child: Text('Answoer 1'),
