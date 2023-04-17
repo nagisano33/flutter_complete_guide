87f1fc2943449ee099d979a479f9063d301fbaab
nagisano33 <vita.stars2@gmail.com>
2023-04-18 00:24:13 +0900

    Update リセットボタンを追加

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = 'You did it!';
@@ -23,9 +24,17 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      resultPhrase,
-      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        child: Column(
+      children: <Widget>[
+        Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        TextButton(
+          child: Text('Restart Quiz!'),
+          onPressed: resetHandler,
+        )
+      ],
     ));
   }
 }
