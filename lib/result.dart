7b5036c2722b6b59cabc6e45ccc69e6fdd567f5a
nagisano33 <vita.stars2@gmail.com>
2023-04-18 00:10:29 +0900

    Update スコアによって表示を変える

--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 99) {
+      resultText = 'You are ... strange?!';
+    } else {
+      resultText = 'you are so bad';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('You did it!'));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+    ));
   }
 }
